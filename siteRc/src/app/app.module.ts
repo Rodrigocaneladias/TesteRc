8b20e4c1ad7a5a21494e2262b2bec4e3f392e9d5
DIR\rodrigo.canela.dias <rodrigo.canela.dias@accenture.com>
2018-09-25 17:59:47 -0300

    tela sobre

--- siteRc/src/app/app.module.ts
+++ siteRc/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule
