df3fefb7fdb5edfbafa4b3611bf5a7728c6471dd
DIR\rodrigo.canela.dias <rodrigo.canela.dias@accenture.com>
2018-10-05 18:23:42 -0300

    Alterando items

--- src/app/catalogo/menu-catalogo/cosmetico/cosmetico.component.ts
+++ src/app/catalogo/menu-catalogo/cosmetico/cosmetico.component.ts
@@ -10,12 +10,14 @@ export class CosmeticoComponent implements OnInit {
 
   produtos: Produto[] = [
     {
+      id:'al',
       nome: 'teste alimenticia',
       descricao: 'descricao alimenticia',
       imagePath: 'https://a2.vnda.com.br/370x370/invictaonline/2018/02/15/101528081808-pote-de-vidro-liso-2-5l-vermelho-velvet-1117.jpg',
       quantidade: 0
     },
     {
+      id:'al2',
       nome: 'teste alimenticia 2',
       descricao: 'descricao alimenticia 2 ',
       imagePath: 'https://a3.vnda.com.br/370x370/invictaonline/2018/02/25/101528470121-pote-de-vidro-quadrado-2l-prata-1215.jpg',
